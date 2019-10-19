f42d50444f4dd19f36bbda5222401ba57c5026a1
7131HDMC <haridasafiuza@gmail.com>
2019-10-19 05:48:01 -0300

    insert don't work

--- colmeia/app/user.php
+++ colmeia/app/user.php
@@ -9,9 +9,5 @@ class user extends Model
     protected $table = "abelha";
     protected $fillable = ['pkEmail','password','indentifyUser', 'callDisponibility','sosMensage'];
  	protected $guarded = ['id', 'created_at', 'update_at'];
- 	public function logIn($email)
- 	{
- 		$email = DB::table('abelha')->where('email', $email);
- 		return 'Ja cadastrado';
- 	}
+ 	
  }
